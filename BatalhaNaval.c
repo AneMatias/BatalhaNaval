771281e99d6070e31008f1f5504e78b8ffdab578
Ane Matias <annye.loves2@gmail.com>
2026-02-05 17:55:02 +0000

    NVL MESTRE

--- BatalhaNaval.c
+++ BatalhaNaval.c
@@ -1,59 +1,99 @@
 #include <stdio.h>
 
 #define TAM 10
-#define TAM_NAVIO 3
-#define NAVIO 3
 #define AGUA 0
-
+#define NAVIO 3
+#define TAM_HABILIDADE 5
 
 int main() {
 
     // TABULEIRO 10X10
     int tabuleiro[TAM][TAM];
-    
-        // AGUA
-        for (int i = 0; i < TAM; i++) {
-            for (int j = 0; j < TAM; j++) {
-                tabuleiro[i][j] = AGUA;
-            }
-        }
+     
+    // MATRIZES
+    int cone[TAM_HABILIDADE][TAM_HABILIDADE];
+    int cruz[TAM_HABILIDADE][TAM_HABILIDADE];
+    int octaedro[TAM_HABILIDADE][TAM_HABILIDADE];
 
-        // NAVIO HORIZONTAL
-        int linha = 1, coluna = 4;
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            tabuleiro[linha][coluna + i] = NAVIO;
-        }
+    int centro = TAM_HABILIDADE / 2; // sera o centro da matriz
 
-        // NAVIO VERTICAL
-        linha = 6, coluna = 6;
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            tabuleiro[linha + i][coluna] = NAVIO;
-        }
-
-        // NAVIO DIAGONAL 1
-        linha = 0, coluna = 0;
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            tabuleiro[linha + i][coluna + i] = NAVIO;
-        }
-
-        // NAVIO DIAGONAL 2
-        linha = 2, coluna = 8;
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            tabuleiro[linha + i][coluna - i] = NAVIO;
-        }
-
-        // IMPRIMIR O TABULEIRO
-        printf("\n===== TABULEIRO NAVAL =====\n");
-        printf("\n");
-        for (int i = 0; i < TAM; i++) {
-            for (int j = 0; j < TAM; j++) {
-                printf("| %d ", tabuleiro[i][j]);
-    
+    // CONE
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            if (i <= centro && j >= centro - i && j <= centro + i)
+                cone[i][j] = NAVIO;
+            else
+                cone[i][j] = AGUA;
         }
         
+    }
+
+    // CRUZ
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            if (i == centro || j == centro)
+                cruz[i][j] = NAVIO;
+            else 
+                cruz[i][j] = AGUA;
+        }
+    }
+
+    // OCTAEDRO
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+
+            int dist_linha;
+            int dist_coluna;
+
+            dist_linha = (i - centro);
+            dist_coluna = (j - centro);
+
+            if (i> centro)
+                dist_linha = i - centro;
+            else
+                    dist_linha = centro - i;
+
+            if (j > centro)
+                dist_coluna = j - centro;
+            else
+                dist_coluna = centro - j;
+
+            if (dist_linha + dist_coluna <= centro)
+                octaedro[i][j] = NAVIO;
+            else 
+                    octaedro[i][j] = AGUA;
+                
+        }
+    }
+    
+    // EXIBIR MATRIZES
+    printf("\n=== CONE ===\n");
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            printf("%d ", cone[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n=== CRUZ ===\n");
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            printf("%d ", cruz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n=== OCTAEDRO ===\n");
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            printf("%d ", octaedro[i][j]);
+        }
         printf("\n");
         
     }
+printf("\n");   
+
+
 
     return 0;
 }
