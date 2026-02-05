bb87c22761b629cb77d5e873bafbfbd06eb14f8a
Ane Matias <annye.loves2@gmail.com>
2026-02-05 00:45:04 +0000

    NVL AVENTUREIRO

--- BatalhaNaval.c
+++ BatalhaNaval.c
@@ -1,28 +1,53 @@
 #include <stdio.h>
 
+#define TAM 10
+#define TAM_NAVIO 3
+#define NAVIO 3
+#define AGUA 0
+
+
 int main() {
 
-    int navioHorizontal[3] = {3, 3, 3};
-    int navioVertical[3]   = {3, 3, 3};
+    // TABULEIRO 10X10
+    int tabuleiro[TAM][TAM];
+    
+        // AGUA
+        for (int i = 0; i < TAM; i++) {
+            for (int j = 0; j < TAM; j++) {
+                tabuleiro[i][j] = AGUA;
+            }
+        }
 
-    int batalhaNaval[10][10] = {
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,3,3,3,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,3,0,0,0,3,3,3,0},
-        {0,0,3,0,0,0,0,0,0,0},
-        {0,0,3,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,3,0,0},
-        {0,0,0,0,0,0,0,3,0,0},
-        {0,0,0,0,0,0,0,3,0,0},
-        {0,0,0,0,0,0,0,0,0,0}
-    };
+        // NAVIO HORIZONTAL
+        int linha = 1, coluna = 4;
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            tabuleiro[linha][coluna + i] = NAVIO;
+        }
 
-    printf("\n===== TABULEIRO NAVAL =====\n");
-    printf("\n");
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            printf("| %d ", batalhaNaval[i][j]);
+        // NAVIO VERTICAL
+        linha = 6, coluna = 6;
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            tabuleiro[linha + i][coluna] = NAVIO;
+        }
+
+        // NAVIO DIAGONAL 1
+        linha = 0, coluna = 0;
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            tabuleiro[linha + i][coluna + i] = NAVIO;
+        }
+
+        // NAVIO DIAGONAL 2
+        linha = 2, coluna = 8;
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            tabuleiro[linha + i][coluna - i] = NAVIO;
+        }
+
+        // IMPRIMIR O TABULEIRO
+        printf("\n===== TABULEIRO NAVAL =====\n");
+        printf("\n");
+        for (int i = 0; i < TAM; i++) {
+            for (int j = 0; j < TAM; j++) {
+                printf("| %d ", tabuleiro[i][j]);
     
         }
         
